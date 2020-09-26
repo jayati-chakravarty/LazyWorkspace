e26cfd87f6b3af6a1676a6369feedea71f3dafb7
Jayati Chakravarty <jayati0801@gmail.com>
2020-09-26 12:55:14 +0530

    All the options are now working properly

--- GUI_design.py
+++ GUI_design.py
@@ -2,18 +2,20 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import GUI_function as BF
 from tkinter import Menu,filedialog
+from os import getcwd
 
 root = tk.Tk()
 root.title('Workspace Setter')
+root.maxsize(500,800)
 
 canvas = tk.Canvas(root, height=500, width=500, bg="white")
 canvas.pack()
-# canvas.place(relwidth=0.8,relheight=0.5,relx=0.1,rely=0.1)
+
 
 #Set the background for canvas
 def setbackground():
     print('in change bg')
-    bk_image = filedialog.askopenfile(initialdir = "/home/jc/Resume_Projects/App_Opener_Advanced",title = "Choose Background Image",
+    bk_image = filedialog.askopenfile(initialdir = getcwd(),title = "Choose Background Image",
     filetypes = (("image files","*.jpg"),("all files","*.*")))
 
     print(str(bk_image.name))
@@ -30,18 +32,15 @@ frame.place(relwidth=0.8,relheight=0.5,relx=0.1,rely=0.1)
 label1 = tk.Label(frame,text="Your workspace :")
 label1.pack()
 
-
 menubar = Menu(root)
 
 filemenu = Menu(menubar)
 filemenu.add_command(label='Save Workspace', command = BF.saveFile)
 filemenu.add_command(label='Load Workspace', command = BF.loadFile)
-filemenu.add_command(label='Modify Default Location')
 menubar.add_cascade(label="Workspace",menu=filemenu)
 
 settings = Menu(menubar)
 settings.add_command(label='Set Background',command = setbackground)
-settings.add_command(label='Change default location')
 menubar.add_cascade(label='Settings',menu=settings)
 
 button_clearApps = tk.Button(root,text="Clear Set Apps",fg="black",bg="#f7cac9",command=BF.clearapps)
@@ -54,5 +53,6 @@ button_openFile = tk.Button(root,text="Set my Workspace",fg="black",bg="#f7cac9"
 button_openFile.pack(side=tk.RIGHT)
 
 root.config(menu=menubar)
+BF.setscreen('')
 root.mainloop()
 
--- GUI_function.py
+++ GUI_function.py
@@ -6,7 +6,17 @@ import time
 from PIL import ImageTk, Image
 
 apps = []
-loadFilename = '/home/jc/Resume_Projects/App_Opener_Advanced/save.txt'
+loadFilename = os.getcwd()+'/save.txt'
+load_has_been_called = False
+
+def fill_apps(loadfilename1):
+    global apps
+    with open(loadfilename1,'r')as f:
+        tempapps = f.read()
+        tempapps = tempapps.split(',')
+        if os.path.isfile(loadfilename1):
+            apps = [x for x in tempapps if x.strip()]
+
 
 def display_app_list(apps):
     print('in display app list')
@@ -14,39 +24,45 @@ def display_app_list(apps):
         label = tk.Label(at.frame,text=app,bg="gray")
         label.pack()
 
-def setscreen(loadFilename):
+def setscreen(loadFilename1):
     print('in set screen')
-    
-    if loadFilename == '' or os.stat(loadFilename).st_size == 0 :
-        print('Using default file')
-        loadFilename = '/home/jc/Resume_Projects/App_Opener_Advanced/save.txt'
+    global apps
+    global loadFilename
+
+    if load_has_been_called == True:
+        if loadFilename1 != '' or os.stat(loadFilename1).st_size > 0 :
+            print('Using file : ',loadFilename1)
+            loadFilename = loadFilename1
+            for widget in at.frame.winfo_children(): 
+                if widget not in(at.label1):
+                    widget.destroy()
         
-    else : 
-        print('Using file : ',loadFilename)
-        clearapps()
-    
-    with open(loadFilename,'r')as f:
-        tempapps = f.read()
-        tempapps = tempapps.split(',')
-        if os.path.isfile(loadFilename):
-            apps = [x for x in tempapps if x.strip()]
+    else :
+        print('Using default file',loadFilename)
+        loadFilename = os.getcwd()+'/save.txt'
+        if os.path.isfile(loadFilename) == False:
+            with open(loadFilename, 'w') as f: 
+                pass
+
+    fill_apps(loadFilename)     
+    print(apps)  
     
     display_app_list(apps)
-
+    
 
 #add apps to be opened
 def addApp():
     print('in addApp')
     print(apps)
     for widget in at.frame.winfo_children(): 
-        if widget != at.label1:
+        if widget != at.label1 :
             widget.destroy()
 
     filename=filedialog.askopenfilename(initialdir="/",title="Select File")
     apps.append(filename)
     print(apps)
     display_app_list(apps)
-    save_workspace()
+    save_workspace(loadFilename)
 
 
 #open the selected at.apps
@@ -59,34 +75,46 @@ def runapps():
             opener ="open" if sys.platform == "darwin" else "xdg-open"
             subprocess.call([opener, app])
 
+#Clear the workspace 
 def clearapps():
     print('in clear app')
-    # if save.txt is not empty, create a copy of save.txt suffixed with mmddhhMM
-    # else remove save.txt
-    savefilename = '/home/jc/Resume_Projects/App_Opener_Advanced/save.txt'
-    if os.path.isfile(savefilename):
-        print('File Found',savefilename)
-        if os.stat(savefilename).st_size !=0 :
-            print('in if loop')
-            ts1 = time.gmtime()
-            ts1 = str(ts1.tm_mday)+str(ts1.tm_mon)+str(ts1.tm_hour)+str(ts1.tm_min)
-            new_file_name = '/home/jc/Resume_Projects/App_Opener_Advanced/save'+ts1+'.txt'
-            print(new_file_name)
-            os.rename('/home/jc/Resume_Projects/App_Opener_Advanced/save.txt',new_file_name)
-        else:
-            print('in else loop')
-            os.remove('/home/jc/Resume_Projects/App_Opener_Advanced/save.txt')
+    if load_has_been_called == False:
+        savefilename = os.getcwd()+'/save.txt'
+        print(savefilename)
+        if os.path.isfile(savefilename):
+            print('File Found',savefilename)
+            if os.stat(savefilename).st_size !=0 :
+                print('in if loop')
+                ts1 = time.gmtime()
+                ts1 = str(ts1.tm_mday)+str(ts1.tm_mon)+str(ts1.tm_hour)+str(ts1.tm_min)
+                new_file_name = os.getcwd()+'/save'+ts1+'.txt'
+                print(new_file_name)
+                os.rename(savefilename,new_file_name)
+            else:
+                print('in else loop')
+                os.remove(savefilename)
 
-    #remove the commands from the screen 
     apps.clear()
 
     for widget in at.frame.winfo_children(): 
-        if widget != at.label1:
+        if widget not in (at.label1) :
             widget.destroy()
+ 
 
-def save_workspace():
+def save_workspace(savefilename):
     print('in save workspace')
-    with open('/home/jc/Resume_Projects/App_Opener_Advanced/save.txt','w') as f:
+    print(savefilename)
+    savefile_default = os.getcwd()+'/save.txt'
+
+    if savefilename != '' and savefilename != savefile_default:
+        if os.stat(savefilename).st_size > 0 :
+            savefile_default = savefilename
+            mode = 'a+'
+    else :
+        mode = 'w'
+    
+    with open(savefile_default,mode) as f:
+        print('writing apps to',savefile_default)
         for app in apps:
             f.write(app+',')
 
@@ -100,10 +128,12 @@ def saveFile():
             f.write(app+',')
 
 def loadFile():
+    global load_has_been_called
+    load_has_been_called = True
     print('in load file')
     loadFilename=filedialog.askopenfilename(initialdir = "/home/jc/Resume_Projects/App_Opener_Advanced",title = "Load_Workspace",
     filetypes = (("text files","*.txt"),("all files","*.*")))
     setscreen(loadFilename)
 
-
+# setscreen(loadFilename)
 
